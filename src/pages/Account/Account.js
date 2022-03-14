040e23a3aea8065e1d121a5910819d691ead8554
hoangtinh997 <hoangtinhqk@gmail.com>
2022-03-14 13:55:53 +0700

    [commit 14/3] balance account

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -2,31 +2,67 @@ import { Table, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState} from 'react';
 import Deposit from '../../Components/Header/Buttons/Deposit'
+import { getConfig } from '../../services/config';
+
+const config = getConfig('testnet')
 
 const Account = () => {
     const [result, setResult] = useState([])
-    
+    let obj = {};
     const contract = window.contract;
     const accountId = window.accountId;
 
     const getBalanceOf = async () => {
-        const depo = await contract.get_deposits({ account_id: accountId })
-        
-        for (let i in depo) {
+        let depo = await contract.get_deposits({ account_id: accountId })
+        const tokens = await fetch(
+            `${config.helperUrl}/account/${accountId}/likelyTokens`
+          )
+            .then((response) => response.json())
+            .then((tokens) => tokens);
+
+        for (let i of tokens) {
+            let balanceOfWallet  = await window.walletConnection.account().viewFunction(i, "ft_balance_of", {account_id: accountId})
+                .catch((err) => {
+                    return {
+                        isFailed: true
+                    }
+                });
+            if(balanceOfWallet.isFailed) {
+                continue
+            }
+            
             let metaData = await window.walletConnection.account().viewFunction(i, "ft_metadata")
-            const obj = {
+
+             obj = {
                 id: i,
                 name: metaData.name,
+                balanceAccount : balanceOfWallet,
                 symbol: metaData.symbol,
-                decimals: metaData.decimals,
-                balance: depo[i]
-            };
-            setResult(data => [...data,obj]);
+            }
+
+            for (let i2 in depo) {
+                if (i === i2) {
+                    obj.balanceContract = depo[i]
+                }
+            }
+            setResult((data) => [...data, obj]);
         }
+        
+        // for (let i in depo) {
+        //     let metaData = await window.walletConnection.account().viewFunction(i, "ft_metadata")
+        //     const obj = {
+        //         id: i,
+        //         name: metaData.name,
+        //         symbol: metaData.symbol,
+        //         decimals: metaData.decimals,
+        //         balance: depo[i]
+        //     };
+        //     setResult(data => [...data,obj]);
+        // }
     }
 
     useEffect(() => {
-        getBalanceOf();
+        getBalanceOf()
         // await axios.get("http://localhost:5000/token").then(rep => console.log('rep', rep.data));
         // await axios.post("http://localhost:5000/token", {
         //     name: "Haha token",
@@ -51,7 +87,8 @@ const Account = () => {
                         <th>Symbol</th>
                         <th>Token name</th>
                         <th>Token ID</th>
-                        <th>Amount</th>
+                        <th>Amount in wallet Account</th>
+                        <th>Amount in Contract</th>
                         <th>Deposit</th>
                     </tr>
                 </thead>
@@ -63,7 +100,8 @@ const Account = () => {
                                     <td>{item.symbol}</td>
                                     <td>{item.name}</td>
                                     <td>{item.id}</td>
-                                    <td>{item.balance * 10 **-8}</td>
+                                    <td>{item.balanceAccount ? item.balanceAccount * 10 **-8 : 0}</td>
+                                    <td>{item.balanceContract ? item.balanceContract * 10 **-8 : 0}</td>
                                     <td>
                                         <Deposit item={item}/>
                                     </td>
