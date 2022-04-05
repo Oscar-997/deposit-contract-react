ee0ca6edce1c0248c8929fc3364cf5b7d9f596cb
hoangtinh997 <hoangtinhqk@gmail.com>
2022-04-05 13:46:48 +0700

    [commit 5/4] viewPool

--- src/Components/Header/Buttons/ButtonViewAccount.js
+++ src/Components/Header/Buttons/ButtonViewAccount.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
-import styled from 'styled-components'; 
+import styled from 'styled-components';
 
 const WrapButtonPool = styled.div`
     position: relative;
@@ -20,60 +20,80 @@ const WrapButtonPool = styled.div`
 const MenuDropdown = styled.div`
     display: none;
     position: absolute;
-    background-color: rgba(21, 61, 111, 0.44);
+    background-color: #525E75;
     border: 1px solid rgba(21, 61, 111, 0.44);
+    left: -80%;
     width: 200px;
     min-width: 160px;
     box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    color: white;
     z-index: 1;
     border-radius: 10px;
     margin-top: 4px;
+
+
+    
+
+    a {
+        color: #FFF6EA;
+        text-decoration: none;
+    }
+
+    div:first-child {
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+    }
+
+    div:last-child {
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+    }
 `
 
-const ItemDropdown = styled.a`
+const ItemDropdown = styled.div`
     padding: 12px 16px;
-    text-decoration: none;
     display: block;
     cursor: pointer;
     &:hover {
-        background-color: rgba(21, 61, 111, 1);
+        background-color: #FFF6EA;
+    }
+    &:hover a {
+        color: #3A3845;
     }
 `
 
 const ButtonViewAccount = () => {
     return (
-    <>
-        <Button variant="danger">
-            <Link to={'/'} style={{color: 'white', textDecoration: 'none'}}>
-                Home
-            </Link>
-        </Button>
-        <Button variant="success">
-            <Link to={'/account'} style={{color: 'white', textDecoration: 'none'}}>
-                Account Details
-            </Link>
-        </Button>
-        <Button variant="warning">
-            <Link to={'/swap'} style={{color: 'black', textDecoration: 'none'}}>
-                Swap
-            </Link>
-        </Button>
-        <WrapButtonPool>
-            <Button variant="info">Pool</Button>
-            <MenuDropdown className='pool-list'>
-                <ItemDropdown>
-                    <Link to={'/create-pool'}>Create new pool</Link>
-                </ItemDropdown>
-                <ItemDropdown>
-                    <Link to={'/add-liquidity'}>Add liquidity</Link>
-                </ItemDropdown>
-                <ItemDropdown>
-                    <Link to={'/view-pools'}>View pools</Link>
-                </ItemDropdown>
-            </MenuDropdown>
-        </WrapButtonPool>
-    </>
+        <>
+            <Button variant="danger">
+                <Link to={'/'} style={{ color: 'white', textDecoration: 'none' }}>
+                    Home
+                </Link>
+            </Button>
+            <Button variant="success">
+                <Link to={'/account'} style={{ color: 'white', textDecoration: 'none' }}>
+                    Account Details
+                </Link>
+            </Button>
+            <Button variant="warning">
+                <Link to={'/swap'} style={{ color: 'black', textDecoration: 'none' }}>
+                    Swap
+                </Link>
+            </Button>
+            <WrapButtonPool>
+                <Button variant="info">Pool</Button>
+                <MenuDropdown className='pool-list'>
+                    <ItemDropdown>
+                        <Link to={'/create-pool'}>Create new pool</Link>
+                    </ItemDropdown>
+                    <ItemDropdown>
+                        <Link to={'/add-liquidity'}>Add liquidity</Link>
+                    </ItemDropdown>
+                    <ItemDropdown>
+                        <Link to={'/view-pools'}>View pools</Link>
+                    </ItemDropdown>
+                </MenuDropdown>
+            </WrapButtonPool>
+        </>
     )
 }
 
--- src/pages/Pool/ViewPools.js
+++ src/pages/Pool/ViewPools.js
@@ -2,61 +2,51 @@ import { useEffect, useContext, useState } from 'react';
 import { Table } from 'react-bootstrap'
 import { BsFillArrowDownCircleFill, BsFillArrowUpCircleFill } from "react-icons/bs";
 import { getConfig } from '../../services/config';
-import {TokenResults, getBalanceOf2} from '../../context/TokenResultsContext'
-
+import { TokenResults, getBalanceOf2 } from '../../context/TokenResultsContext'
+import loading from '../../assets/loading-gift.gif'
+import { getAllByPlaceholderText } from '@testing-library/react';
 const ViewPools = () => {
+    const [allPools, setAllPools] = useState([])
+    const [allMetadata, setAllMetadata] = useState([])
 
     const config = getConfig('testnet');
+    const contract = window.contract;
+    
+    useEffect(async () => {
+        const allPools = await contract.get_pools({ from_index: 0, limit: 100 })
+        console.log("All pools: ", allPools)
+        let nonDupTokenList = [];
+        let tempArr = []
 
-    const { result } = useContext(TokenResults)
-
-    const parsePool = (pool, id, result) => {
-        // console.log("Check result: " , result);
-        // console.log("pool", pool);
-        const allPools = [];
-        let obj = {
-            id: id >= 0 ? id : pool.id,
-            tokenIds: pool.token_account_ids,
-            supplies: pool.amounts.reduce
-                ((acc, amount, i) => {
-                    acc[pool.token_account_ids[i]] = amount;
-                    return acc
-                }, {})
-            ,
-            fee: pool.total_fee,
-            shareSupply: pool.shares_total_supply,
-            tvl: pool.tvl,
-        }
-            for (let i in result) {
-                for (let i2 in pool.token_account_ids) {
-                        if(i.id === i2){
-                            obj.symbolToken = i.symbol;
+        let isInArr = false;
+        allPools.map((pool, i) => {
+            // arr = pool.token_account_ids
+            if (nonDupTokenList.length > 0) { // convert to non-duplicate token_id's array
+                pool.token_account_ids.map((id) => {
+                    for (let i of nonDupTokenList) {
+                        if (id === i){
+                            isInArr = true
                         }
-                }
-                allPools.push(obj)
+                    }
+                    if (!isInArr) {
+                        nonDupTokenList.push(id)
+                    } else {
+                        isInArr = false
+                    }
+                })
+            } else {
+                nonDupTokenList.push(...pool.token_account_ids)
             }
 
-
-        return allPools;
-    }
-
-    const getAllPools = async (result) => {
-        let page = 1;
-        let perPage = 100;
-        const index = (page - 1) * perPage;
-        const poolData = await window.walletConnection.account().viewFunction(config.contractName, 'get_pools', { from_index: index, limit: perPage })
-        return poolData.map((rawPool, i) =>  parsePool(rawPool, i + index, result)) 
-    }
-
-    useEffect(async() => {
-        if (window.walletConnection.isSignedIn()) {
-           
-     
-                const result = await getBalanceOf2();
-                const allPools = await getAllPools(result);
-                console.log(allPools);
-        
-        }
+        })
+        nonDupTokenList.map(async(token_id) => {
+            const metadata = await window.walletConnection.account().viewFunction(token_id, "ft_metadata")
+            tempArr.push(metadata)
+            console.log(tempArr)
+            return metadata
+        })
+        console.log(tempArr)
+        setAllPools(allPools)
     }, [])
 
     return (
@@ -64,20 +54,14 @@ const ViewPools = () => {
             <thead>
                 <tr>
                     <th>#</th>
-                    <th>Pair</th>
+                    <th style={{ minWidth: 250 }}>Pair</th>
                     <th>Fee < BsFillArrowUpCircleFill /></th>
                     <th>TVL <BsFillArrowDownCircleFill /></th>
                     <th>Pools</th>
                 </tr>
             </thead>
             <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                
             </tbody>
         </Table>
     )
